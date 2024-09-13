4849830717bf655204b18308e6747cd5d5edbb18
Andy Huschka <andy_huschka@web.de>
2024-09-13 15:44:04 +0200

    move: operator folders in operators

--- operators/javascript-grouping-operators/script.js
+++ operators/javascript-grouping-operators/script.js
@@ -10,3 +10,12 @@ console.log(7 + 20 / 5);
 
 console.log((7 + 20) / 5);
 // REsult: 5.4
+
+console.log(5 + 5 === 10);
+// Result: true
+
+console.log(2 * 6 < 13);
+// Result: true
+
+console.log(6 / 2 === 1 + 5 * 3 - 13);
+// Result: true
