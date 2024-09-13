d302e0403778b5f1a392eb1e5b048f1ef5d2fedb
Andy Huschka <andy_huschka@web.de>
2024-09-13 14:58:43 +0200

    add folder: javascipt operators

---
+++ javascript-operators/script.js
@@ -0,0 +1,18 @@
+// Single line Comment
+
+/* 
+Das ist ein Multi Line Comment
+Wie man sieht
+*/
+
+// Addition
+console.log(1 + 1);
+
+// Subtraction
+console.log(1 - 1);
+
+// Multiplication
+console.log(3 * 3);
+
+// Division
+console.log(20 / 5);
