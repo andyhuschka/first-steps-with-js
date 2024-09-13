7da34d85dbbc2001bdc5f741261efa5b964ed7ad
Andy Huschka <andy_huschka@web.de>
2024-09-13 15:41:18 +0200

    add folder: logical and grouping

---
+++ javascript-grouping-operators/script.js
@@ -0,0 +1,12 @@
+// Single line Comment
+
+/* 
+Das ist ein Multi Line Comment
+Wie man sieht
+*/
+
+console.log(7 + 20 / 5);
+// Result: 11
+
+console.log((7 + 20) / 5);
+// REsult: 5.4
---
+++ javascript-logical-operators/script.js
@@ -0,0 +1,24 @@
+// Single line Comment
+
+/* 
+Das ist ein Multi Line Comment
+Wie man sieht
+*/
+
+// AND
+console.log(2 > 1 && 5 < 6); // true
+
+console.log(2 > 1 && 7 < 6); // false
+
+// OR
+console.log(2 > 1 || 5 < 6); // true
+
+console.log(2 > 1 || 7 < 6); // true
+
+console.log(2 > 3 || 7 < 6); // false
+
+// NOT
+
+console.log(5 > 10); // false
+
+console.log(!(5 > 10)); // true
