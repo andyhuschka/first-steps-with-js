7c3ebb2001df983432c7248980cddd5837153209
Andy Huschka <andy_huschka@web.de>
2024-09-13 15:49:43 +0200

    add folder: concationation operator

---
+++ operators/javascript-concatenation-operators/script.js
@@ -0,0 +1,18 @@
+// Single line Comment
+
+/* 
+Das ist ein Multi Line Comment
+Wie man sieht
+*/
+
+console.log("My name is Max");
+// Result: "My name is Max"
+
+console.log("My" + " name is Max");
+// Result: "My name is Max"
+
+console.log("My" + " name " + "is Max");
+// Result: "My name is Max"
+
+console.log("My" + " name " + "is " + "Max");
+// Result: "My name is Max"
