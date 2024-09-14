313c8c280ac983ffd35aea90d28c976042de9142
Andy Huschka <andy_huschka@web.de>
2024-09-14 13:22:26 +0200

    add: test operators folder

---
+++ operators/test-javascript-operators/script.js
@@ -0,0 +1,35 @@
+// Single line Comment
+
+/* 
+Das ist ein Multi Line Comment
+Wie man sieht
+*/
+
+// Question 1
+
+console.log("A" + "n" + "d" + "y");
+
+// Question 2
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 2 === 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" > 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+// Question 3
+
+console.log(25 / 5 === 5); // Result: 5
+console.log(50 / 10 === 5); // Result: 5
