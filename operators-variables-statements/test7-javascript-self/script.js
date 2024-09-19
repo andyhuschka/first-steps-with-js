0550e2d8d7b7ac0fc16e31b6456889f5acf185d8
Andy Huschka <andy_huschka@web.de>
2024-09-19 09:38:06 +0200

    add: Java self test

---
+++ operators-variables-statements/test7-javascript-self/script.js
@@ -0,0 +1,28 @@
+// Single line Comment
+
+/* 
+Das ist ein Multi Line Comment
+Wie man sieht
+*/
+
+//--- Test 1 ---//
+
+function logHello() {
+  console.log("hello Users");
+}
+
+logHello();
+
+const helloFn = logHello;
+
+helloFn();
+
+//--- Test 2 ---//
+
+const sayHello = function () {
+  console.log("hello");
+};
+
+sayHello();
+
+//--- Test 3 ---//
